1e16441ad062bf937bcd3f22c0e55bb8156b385d
Richard Green <richardgreen@pursuit.org>
2019-07-29 17:53:24 -0400

    created a home page, added text with gradient background using -webkit-background-clip

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Header from './containers/Header';
 import Layout from './containers/Layout';
+import Main from './containers/Main';
 
 
 function App() {
   return (
     <div className="App">
       <Header /> 
-      <Layout />     
+      {/* <Layout />      */}
+      <Main />
     </div>
   );
 }
---
+++ src/containers/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default()=>{
+    return (
+        <main>
+            <section className='section-about'>
+                <h2 className='heading-secondary'>
+                    Exciting Tours for Adventurous People
+                </h2>
+            </section>
+        </main>
+    )
+}
