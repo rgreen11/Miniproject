6614d810e68e06da0c18f49b96567fdd78002716
Richard Green <richardgreen@pursuit.org>
2019-07-29 18:21:05 -0400

    created margin-bottom class and put it inside _utilities.scss inorder to reuse code

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,11 +4,21 @@ export default()=>{
     return (
         <main>
             <section className='section-about'>
-                <div className='u-center-text'>
+                <div className='u-center-text u-margin-bottom-8'>
                     <h2 className='heading-secondary'>
                         Exciting Tours for Adventurous People
                     </h2>
                 </div>
+
+                <div className="row">
+                    <div className="col-1-of-2"> 
+                        Text content
+                    </div>
+                    <div className="col-1-of-2"> 
+                        image
+                    </div>
+                </div>
+
             </section>
         </main>
     )
