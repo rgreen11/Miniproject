0a70f59f8cbc80156cdacb721948fa6dcaf8ac90
Richard Green <richardgreen@pursuit.org>
2019-07-29 18:12:58 -0400

    added an _ to home scss file

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,9 +4,11 @@ export default()=>{
     return (
         <main>
             <section className='section-about'>
-                <h2 className='heading-secondary'>
-                    Exciting Tours for Adventurous People
-                </h2>
+                <div className='u-center-text'>
+                    <h2 className='heading-secondary'>
+                        Exciting Tours for Adventurous People
+                    </h2>
+                </div>
             </section>
         </main>
     )
