46afc1b0cda0aa5319782dd60ecdcfab816c2730
Richard Green <richardgreen@pursuit.org>
2019-08-02 15:49:00 -0400

    completed Tours page

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,23 +1,23 @@
 import React from 'react';
 // import '../styles/Header.css';
 import '../sass/_main.scss';
-import { BrowserRouter as Router, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Link } from "react-router-dom";
 
 
 
 export default ()=>{
     return(
-        <Router>
+        <div>
             <header className='header'>
                 <div className='header__text-box'>
                     <h1 className='heading-primary'>
                         <p className='heading-primary--main'>Success</p>
                         <p className='heading-primary--sub'>Where Dreams and Goals meet</p>
-                        <Link className='btn btn--white btn--animated' href='#'>click</Link>
+                        <a className='btn btn--white btn--animated' href='#'>click</a>
                     </h1>
                     
                 </div>
             </header>
-        </Router>
+        </div>
     );
 }
--- src/containers/Tours.js
+++ src/containers/Tours.js
@@ -16,30 +16,91 @@ export default ()=>{
                             <div className='card__picture card__picture--1'>
                                 &nbsp;
                             </div>
-                            <div className='card__heading'>
-                                Heading
-                            </div>
+                            <h4 className='card__heading'><span className='card__heading-span card__heading-span--1'>The Sea Explorer</span></h4>
                             <div className='card__details'>
-                                Details
+                                <ul className=''>
+                                    <li>3 day tours</li>
+                                    <li>up to 30 people</li>
+                                    <li>2 tour guides</li>
+                                    <li>Sleeping cozy hotels</li>
+                                    <li>Difficult: Easy</li>
+                                </ul>
                             </div>
                         </div>
                         <div className='card__side card__side--back card__side--back-1'>
-                            Back
+                            <div className='card__cta'>
+                                <div className='card__price-box'>
+                                    <p className='card__price-only'>Only</p>
+                                    <p className='card__price-value'>$297</p>
+                                </div>
+                                <a  href='#'className='btn btn--white'> book Now</a>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-1-of-3"> 
                     <div className='card'>
-                        
+                    <div className='card__side card__side--front'>
+                            <div className='card__picture card__picture--2'>
+                                &nbsp;
+                            </div>
+                            <h4 className='card__heading'><span className='card__heading-span card__heading-span--2'>The Forest Hiker </span></h4>
+                            <div className='card__details'>
+                                <ul className=''>
+                                    <li>7 day tours</li>
+                                    <li>up to 40 people</li>
+                                    <li>6 tour guides</li>
+                                    <li>Sleep in provided tents</li>
+                                    <li>Difficult: Medium</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='card__side card__side--back card__side--back-2'>
+                            <div className='card__cta'>
+                                <div className='card__price-box'>
+                                    <p className='card__price-only'>Only</p>
+                                    <p className='card__price-value'>$497</p>
+                                </div>
+                                <a  href='#'className='btn btn--white'> book Now</a>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
                 <div className="col-1-of-3"> 
                     <div className='card'>
-                        
+                    <div className='card__side card__side--front'>
+                            <div className='card__picture card__picture--3'>
+                                &nbsp;
+                            </div>
+                            <h4 className='card__heading'><span className='card__heading-span card__heading-span--3'>The Snow Adventurer</span></h4>
+                            <div className='card__details'>
+                                <ul className=''>
+                                    <li>5 day tours</li>
+                                    <li>up to 15 people</li>
+                                    <li>3 tour guides</li>
+                                    <li>Sleep in provided tents</li>
+                                    <li>Difficult: Hard</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className='card__side card__side--back card__side--back-3'>
+                            <div className='card__cta'>
+                                <div className='card__price-box'>
+                                    <p className='card__price-only'>Only</p>
+                                    <p className='card__price-value'>$897</p>
+                                </div>
+                                <a  href='#'className='btn btn--white'>book Now</a>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
 
+            <div className='u-center-text u-margin-top-huge'>
+                <a href='#' className='btn btn--green'> Discover all tours</a>
+            </div>
         </section>
     )
 }
