762abe4d8cd1d90b3b39ea78d55a5a41aed7c2f9
Richard Green <richardgreen@pursuit.org>
2019-08-18 13:32:08 -0400

    added video tag used autoPlay muted and loop to continusly play the background video. placed source tag inside with src video

--- src/containers/Stories.js
+++ src/containers/Stories.js
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import video from '../img/video.mp4';
+import videoM from '../img/video.webm';
 
 export default ()=>{
    return(
             <section className='section-stories'>
+                <div className='bg-video'>
+                    <video className='bg-video__content' autoPlay muted loop>
+                        <source src={`${video}`} type='video/mp4'/>
+                        <source src ={`${videoM}`} type='video/webm'/>
+                        Your Browser does not support it!
+                    </video>
+                </div>
                 <div className='u-center-text u-margin-bottom-big'>
                     <h2 className='heading-secondary'>
                     We make people genuinely happy
@@ -38,6 +46,10 @@ export default ()=>{
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec nisl pharetra dolor mollis mattis and some other textnjsh jjsp,nw js. Phasellus nec nisl pharetra dolor mollis mattis and some other textnjsh.
                         </div>
                     </div>
+                </div>
+
+                <div className='u-center-text u-margin-top-huge'>
+                    <a href='#' className=' btn-text'>Read all stories &rarr;</a>
                 </div>
             </section>
    ) 
